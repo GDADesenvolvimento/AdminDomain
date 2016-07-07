c0f03cd14798dbcbf14afd0a3285f7a44264a988
Diogo Bemfica <diogo.fragabemfica@gmail.com>
2016-07-06 22:40:17 -0300

    Correções nas rotas

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,10 @@
 <?php
 Route::group(['middleware' => ['web', 'auth']], function () {
     Route::get('/domains', ['as' => 'domains', 'uses' => '\GdaDesenv\AdminDomain\Controllers\DomainController@index']);
-    Route::get('/domain/form', ['as' => 'domain.form', 'uses' => '\GdaDesenv\AdminDomain\DomainController@form']);
-    Route::post('/domain/create', ['as' => 'domain.create', 'uses' => '\GdaDesenv\AdminDomain\DomainController@create']);
-    Route::get('/domain/edit/{id}', ['as' => 'domain.edit', 'uses' => '\GdaDesenv\AdminDomain\DomainController@edit']);
-    Route::put('/domain/put', ['as' => 'domain.update', 'uses' => '\GdaDesenv\AdminDomain\DomainController@update']);
-    Route::get('/domain/delete/{id}', ['as' => 'domain.delete', 'uses' => '\GdaDesenv\AdminDomain\DomainController@delete']);
-    Route::post('/domain/consultwhois', ['as' => 'domain.whois', 'uses' => '\GdaDesenv\AdminDomain\DomainController@consultWhois']);
+    Route::get('/domain/form', ['as' => 'domain.form', 'uses' => '\GdaDesenv\AdminDomain\Controllers\DomainController@form']);
+    Route::post('/domain/create', ['as' => 'domain.create', 'uses' => '\GdaDesenv\AdminDomain\Controllers\DomainController@create']);
+    Route::get('/domain/edit/{id}', ['as' => 'domain.edit', 'uses' => '\GdaDesenv\AdminDomain\Controllers\DomainController@edit']);
+    Route::put('/domain/put', ['as' => 'domain.update', 'uses' => '\GdaDesenv\AdminDomain\Controllers\DomainController@update']);
+    Route::get('/domain/delete/{id}', ['as' => 'domain.delete', 'uses' => '\GdaDesenv\AdminDomain\Controllers\DomainController@delete']);
+    Route::post('/domain/consultwhois', ['as' => 'domain.whois', 'uses' => '\GdaDesenv\AdminDomain\Controllers\DomainController@consultWhois']);
 });
